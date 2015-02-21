a7853042d7c5ac959b57afafee94b304b8567b37
Martin Krasser <krasserm@googlemail.com>
2015-02-21 08:39:04 +0100

    Allow consumers to specify event decoder class

--- akka-analytics-kafka/src/main/scala/akka/analytics/kafka/package.scala
+++ akka-analytics-kafka/src/main/scala/akka/analytics/kafka/package.scala
@@ -1,5 +1,7 @@
 package akka.analytics
 
+import scala.reflect.ClassTag
+
 import akka.persistence.kafka.{DefaultEventDecoder, Event}
 
 import org.apache.spark.storage.StorageLevel
@@ -7,11 +9,14 @@ import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.kafka.KafkaUtils
 
-import _root_.kafka.serializer.StringDecoder
+import _root_.kafka.serializer.{Decoder, StringDecoder}
 
 package object kafka {
   implicit class JournalStreamingContext(context: StreamingContext) {
     def eventStream(kafkaParams: Map[String, String], kafkaTopics: Map[String, Int], storageLevel: StorageLevel = StorageLevel.MEMORY_AND_DISK_SER_2): DStream[Event] =
-      KafkaUtils.createStream[String, Event, StringDecoder, DefaultEventDecoder](context, kafkaParams, kafkaTopics, storageLevel).map(_._2)
+      eventStreamWithDecoder[DefaultEventDecoder](kafkaParams, kafkaTopics, storageLevel)
+
+    def eventStreamWithDecoder[A <: Decoder[Event] : ClassTag](kafkaParams: Map[String, String], kafkaTopics: Map[String, Int], storageLevel: StorageLevel = StorageLevel.MEMORY_AND_DISK_SER_2): DStream[Event] =
+      KafkaUtils.createStream[String, Event, StringDecoder, A](context, kafkaParams, kafkaTopics, storageLevel).map(_._2)
   }
 }
